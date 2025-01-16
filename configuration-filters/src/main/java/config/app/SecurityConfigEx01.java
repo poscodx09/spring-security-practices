ebabe79d3695e8f82db4febfdf2607b6ab598fd5
ymj07168 <ymj07168@gmail.com>
2025-01-16 15:06:28 +0900

    configuration-filter

--- configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
+++ configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
@@ -1,5 +1,4 @@
-package config.web;
-
+package config.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,24 +7,33 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigEx01 {
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return new WebSecurityCustomizer() {
-            @Override
-            public void customize(WebSecurity web) {
-                web
-                    .ignoring()
-                    .requestMatchers(new AntPathRequestMatcher("/assets/**"));
-            }
-        };
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
-    }
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return new WebSecurityCustomizer() {
+			@Override
+			public void customize(WebSecurity web) {
+				web.ignoring().requestMatchers(new AntPathRequestMatcher("/assets/**"));
+			}
+		};
+	}
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	   	/*
+    	http
+    		.formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+				@Override
+				public void customize(FormLoginConfigurer<HttpSecurity> t) {
+				}
+    		});
+    	*/
+    	
+    	http
+    		.formLogin((formLogin) -> {
+    		});
+    	
+		return http.build();
+	}
 }
